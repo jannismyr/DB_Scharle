925396743d1890495dce735097f92a26b78ad639
Hoffi14 <S.hofmann03@web.de>
2024-03-12 12:18:38 +0100

    outsourcen des clients

--- backend/src/server.mjs
+++ backend/src/server.mjs
@@ -1,40 +1,12 @@
 import express from 'express';
-import {MongoClient} from 'mongodb'
+//import {MongoClient} from 'mongodb'
+import { client } from './db.mjs'
 
 const port = 8000;
 const app = express();
 
-
-
-async function start(){
-
-    const url = 'mongodb://localhost:27017'
-    const client = new MongoClient(url)
-    
-    await client.connect();
-    const db = client.db('Test_Jannis');
-
-    app.get('/users', async (req,res) => {
-        const users = await db.collection('User').find({}).toArray();
-        res.json(users)
-    })
-    
-    app.get('/users/:Id', async (req,res) => {
-        try {
-            const userId = req.params.Id; // Achte darauf, dass dies zu params.id geändert wurde
-            console.log(userId);
-    
-            const users = await db.collection('User').findOne({ _id: userId });
-    
-            res.send(users);
-        } catch (error) {
-            console.error(error);
-            res.status(500).send('Internal Server Error');
-        }
-    })
-
-}
-
+await client.connect();
+const db = client.db('Test_Jannis');
 
 app.use(express.json());
 app.listen(port,() => {
@@ -45,4 +17,26 @@ app.get('/', (req,res) => {
     res.send('Hallo')
 })
 
-start();
+app.route('/users')
+.get(async (req,res) => {
+    const users = await db.collection('User').find({}).toArray();
+    res.json(users)
+})
+
+
+
+app.get('/users/:Id', async (req,res) => {
+    try {
+        const userId = req.params.Id; // Achte darauf, dass dies zu params.id geändert wurde
+        console.log(userId);
+
+        const users = await db.collection('User').findOne({ _id: userId });
+
+        res.send(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+})
+
+
