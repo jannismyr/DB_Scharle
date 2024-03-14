007aba4f4e0df4f3363f8a45467e8f2f6410ae99
Jannis <87652452+jannismyr@users.noreply.github.com>
2024-03-14 14:22:32 +0100

    erste Seite

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import * as VueRouter from 'vue-router';
+import StartSeite from './seiten/StartSeite.vue';
 
-createApp(App).mount('#app')
+createApp(App)
+.use(VueRouter.createRouter({
+    history: VueRouter.createWebHistory(process.env.BASE_URL),
+    routes: [
+        { path: '/startseite', 
+        component: StartSeite },
+    ]
+}))
+.mount('#app')
---
+++ frontend/src/seiten/StartSeite.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <!-- Hier kommt der HTML-Inhalt der Seite -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'StartSeite',
+    data() {
+        return {
+            // Hier kommen die Daten der Seite
+        };
+    },
+    methods: {
+        // Hier kommen die Methoden der Seite
+    },
+    mounted() {
+        // Hier können Sie Code ausführen, der nach dem Rendern der Seite ausgeführt werden soll
+    }
+};
+</script>
