85fa02e28a9fb4d822714a4fc493fc8ac9a12e3a
Hoffi14 <S.hofmann03@web.de>
2024-03-16 13:40:49 +0100

    Json zu Straftaten hinzugefügt

--- backend/src/data.js
+++ backend/src/data.js
@@ -1,26 +1,89 @@
 const Users= [
     {
-        "Id": "1",
+        "__id": "1",
         "Name": "Simon"
     },
     {
-        "Id": "2",
+        "_id": "2",
         "Name": "Julian"
     },
     {
-        "Id": "3",
+        "_id": "3",
         "Name": "Tom"
     },
     {
-        "Id": "4",
+        "_id": "4",
         "Name": "Fritz"
     },
     {
-        "Id": "5",
+        "_id": "5",
         "Name": "Janni"
     }
 ]
 
 const Fälle = [
     
+]
+
+const Straftaten = [
+    {
+        "_id": "1",
+        "Name": "Mord"
+    },
+    {
+        "_id": "2",
+        "Name": "Diebstahl"
+    },
+    {
+        "_id": "3",
+        "Name": "Raub"
+    },
+    {
+        "_id": "4",
+        "Name": "Körperverletzung"
+    },
+    {
+        "_id": "5",
+        "Name": "Beleidigung"
+    },
+    {
+        "_id": "6",
+        "Name": "Sachbeschädigung"
+    },
+    {
+        "_id": "7",
+        "Name": "Widerstand gegen Vollstreckungsbeamte"
+    },
+    {
+        "_id": "8",
+        "Name": "Unerlaubtes Entfernen vom Unfallort"
+    },
+    {
+        "_id": "9",
+        "Name": "Vortäuschen einer Straftat"
+    },
+    {
+        "_id": "10",
+        "Name": "Menschenhandel"
+    },
+    {
+        "_id": "11",
+        "Name": "Geldwäsche"
+    },
+    {
+        "_id": "12",
+        "Name": "Computerbetrug"
+    },
+    {
+        "_id": "13",
+        "Name": "Urkundenfälschung"
+    },
+    {
+        "_id": "14",
+        "Name": "Brandstiftung"
+    },
+    {
+        "_id": "15",
+        "Name": "Bestechung"
+    }
 ]
