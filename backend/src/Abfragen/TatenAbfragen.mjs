b10aa98479b731677be17080b61a80aad6e74270
Hoffi14 <S.hofmann03@web.de>
2024-03-21 14:03:07 +0100

    Erstelleung/ Verbesserung von Tatbestands Abfragen

--- backend/src/Abfragen/TatenAbfragen.mjs
+++ backend/src/Abfragen/TatenAbfragen.mjs
@@ -5,6 +5,7 @@ const router = express.Router()
 const db = client.db('Test_Jannis');
 
 
+
 router.route('/')
 .get(async (req,res) => {
     try {
@@ -15,32 +16,68 @@ router.route('/')
     }
 })
 .post(async (req,res) => {
-
+    try {
+        const Rolle = req.body.rolle
+        
+        if (Rolle === "Admin") {
+            const Tat = await db.collection('Crime').find().sort({ _id: -1 }).limit(1).toArray();
+            const newId = Tat[0]._id + 1
+            const {Name} = req.body
+            await db.collection('Crime').insertOne({
+                _id: newId,
+                Name: Name
+            })
+            .then(
+                res.status(201).send('Tatbestand hinzugefügt')
+            )
+        } else {
+            res.status(403).send("Nicht für diese Aktion authorisiert");
+        }
+    } catch (error) {
+        console.log(error) 
+    }
 })
 
 
 router.route('/:Id')
 .patch(async (req,res) => {
     try {
+        const Rolle = req.body.rolle
+        if (Rolle === "Admin") {
+
         const {Name} = req.body.Update
-        const CrimeId = req.params.Id;
-        console.log(CrimeId)
+        const CrimeId = parseInt(req.params.Id);
         
-        await db.collection('Case').updateOne({_id: userId}, {$set:{ Name: Name}})
+        await db.collection('Crime').updateOne({_id: CrimeId}, {$set:{ Name: Name}})
 
                 /*auditLog.write("Die Veranstaltung "+GesuchteVeranstaltung['Titel']+ " wurde am "
                 +Datum.toLocaleDateString()+" um "+ Datum.getHours()+":" +Datum.getMinutes()+ " aktualisiert. \n"+ 
                 "--> Das Attribut "+ key+ " wurde von " + alteDaten+ " zu "+ GesuchteVeranstaltung[key]+ " geändert \n")*/
 
-        res.status(200).send('Fall bearbeitet')
-
+        res.status(200).send('Tatbestand bearbeitet')
+        } else {
+            res.status(403).send("Nicht für diese Aktion authorisiert");
+        }
     } catch (error) {
        console.log(error) 
     }
 })
 
 .delete(async (req,res) => {
-
+    try {
+        const Rolle = req.body.rolle
+        const CrimeId = parseInt(req.params.Id);
+    
+        if (Rolle === "Admin") {
+            await db.collection('Crime').deleteOne({_id: CrimeId})
+            res.status(200).send("Tatbestand gelöscht");
+            
+        } else {
+            res.status(403).send("Nicht für diese Aktion authorisiert");
+        }
+    } catch (error) {
+        console.log(error) 
+    }
 })
 
 
