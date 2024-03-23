f9d99d750a3c61b85e5a382ba900c57a2f4dbd1b
Hoffi14 <S.hofmann03@web.de>
2024-03-23 16:41:45 +0100

    Suchfunktion und Hinzufügen von Beweisen programmiert

--- backend/src/Abfragen/FallAbragen.mjs
+++ backend/src/Abfragen/FallAbragen.mjs
@@ -80,8 +80,8 @@ router.route('/')
     const {Tatvorwurf} = req.body
     const {Erfasser_ID, ErfasserName} = req.body.Erfasser
     const {VNameOpfer, NNameOpfer, AWN_Opfer, AdresseOpfer,TelNumOpfer} = req.body.Opfer
-    const {VNameTaeter, NNameTaeter, AWN_Taeter, Tatort,Tatzeit, Tatwaffe} = req.body.Taeter
-    const {Bundesland, Landkreis, Ort} = req.body.Ort
+    const {VNameTaeter, NNameTaeter, AWN_Taeter,Tatzeit} = req.body.Taeter
+    const {Bundesland, Landkreis, Ort, Tatort} = req.body.Ort
 
     let Aktenzeichen = newAktenzeichen(Bundesland)
 
@@ -90,6 +90,7 @@ router.route('/')
         await db.collection('Case').insertOne({
             _id: Aktenzeichen,
             Tatvorwurf: Tatvorwurf,
+            Tatzeit: Tatzeit,
             Erfasser: {
                 Erfasser_ID: Erfasser_ID,
                 ErfasserName: ErfasserName
@@ -104,16 +105,26 @@ router.route('/')
             Taeter:{
               VNameTaeter: VNameTaeter,
               NNameTaeter: NNameTaeter,
-              AWN_Taeter: AWN_Taeter,
-              Tatort: Tatort,
-              Tatzeit: Tatzeit,
-              Tatwaffe: Tatwaffe
+              AWN_Taeter: AWN_Taeter
             },
             Ort: {
                 Bundesland: Bundesland,
                 Landkreis: Landkreis,
-                Ort: Ort
-            }
+                Ort: Ort,
+                Tatort: Tatort
+            },
+            Beweise: [{
+                Gegenstaende:[
+
+                ],
+                Bilder: [
+
+                ],
+                Zeugen:[
+
+                ]
+            }],
+            VerknuepfteFaelle:[]
         })
         .then(
             res.status(201).send('Fall hinzugefügt')
@@ -124,11 +135,39 @@ router.route('/')
     }
 })
 
+router.get('/Suche', async (req,res) => {
+    try {
+        const {Suche} = req.query
+
+        const searchedCase = await db.collection('Case').find({
+            $or: [
+                {_id: {$regex: Suche, $options: 'i'}},
+                {'Ort.Bundesland': {$regex: Suche, $options: 'i'}},
+                {'Ort.Bundesland': {$regex: Suche, $options: 'i'}},
+                {'Ort.Ort': {$regex: Suche, $options: 'i'}},
+                {'Ort.Tatort': {$regex: Suche, $options: 'i'}},
+                {'Opfer.VNameOpfer':{$regex: Suche, $options: 'i'}},
+                {'Opfer.NNameOpfer': {$regex: Suche, $options: 'i'}},
+                {'Opfer.AWN_Opfer': {$regex: Suche, $options: 'i'}},
+                {'Taeter.VNameTaeter': {$regex: Suche, $options: 'i'}},
+                {'Taeter.NNameTaeter': {$regex: Suche, $options: 'i'}},
+                {'Taeter.AWN_Taeter': {$regex: Suche, $options: 'i'}},
+            ] 
+        }).toArray()
+
+        res.status(200).send(searchedCase)
+
+    } catch (error) {
+        console.error(error);
+        res.status(404).send("Fehler bei der Suche")
+    }
+})
+
 router.route('/:Aktenzeichen')
 .get(async (req,res) => {
     try {
         const CaseId = req.params.Aktenzeichen;
-        const searchedCase = await db.collection('Case').findOne({ Aktenzeichen: CaseId });
+        const searchedCase = await db.collection('Case').findOne({ _id: CaseId });
         res.status(200).send(searchedCase);
     } catch (error) {
         console.error(error);
@@ -139,10 +178,10 @@ router.route('/:Aktenzeichen')
     try {
         const body = req.body.Update
         const CaseId = req.params.Aktenzeichen;
-        const searchedCase = await db.collection('Case').findOne({ Aktenzeichen: CaseId });
+        const searchedCase = await db.collection('Case').findOne({ _id: CaseId });
         for (const key in body) {
             if (Object.hasOwnProperty.call(searchedCase, key)) {
-                await db.collection('Case').updateOne({_id: userId}, {$set:{ [key]: body[key]}})
+                await db.collection('Case').updateOne({_id: CaseId}, {$set:{ [key]: body[key]}})
 
                 /*auditLog.write("Die Veranstaltung "+GesuchteVeranstaltung['Titel']+ " wurde am "
                 +Datum.toLocaleDateString()+" um "+ Datum.getHours()+":" +Datum.getMinutes()+ " aktualisiert. \n"+ 
@@ -152,6 +191,7 @@ router.route('/:Aktenzeichen')
         res.status(200).send('Fall bearbeitet')
     } catch (error) {
         console.log(error.message)
+        res.status(404).send("Fehler beim Update")
     }
 })
 .delete(async (req,res) => {
@@ -160,22 +200,91 @@ router.route('/:Aktenzeichen')
         const CaseId = req.params.Aktenzeichen;
     
         if (Rolle === "Admin") {
-            await db.collection('Case').deleteOne({Aktenzeichen: CaseId})
+            await db.collection('Case').deleteOne({_id: CaseId})
             res.status(200).send("Fall gelöscht");
         } else {
             res.status(403).send("Nicht für diese Aktion authorisiert");
         }
         } catch (error) {
             console.log(error.message)
+            res.status(404).send("Fehler beim Löschen aufgetreten")
         }
 })
 
 router.post('/:Aktenzeichen/Beweise', async (req,res) => {
+    try {
+        const CaseId = req.params.Aktenzeichen;
+        const searchedCase = await db.collection('Case').findOne({ _id: CaseId });
+        const query = req.query.Art
+        
+        switch (query) {
+            case "Gegenstand":
+                const {GArt, SRN, Marke, GRelevanz} = req.body
+                await db.collection('Case').updateOne({_id: CaseId}, {$push:{'Beweise.0.Gegenstaende': {
+                    GegenstandsArt: GArt,
+                    Seriennummer: SRN,
+                    Marke: Marke,
+                    Relevanz: GRelevanz
+                }}}).then(
+                    res.status(201).send("Gegenstand zu Fall hinzugefügt")
+                )
+                break;
+            case "Bild":
+                const {Bez, kurzbeschreibung} = req.body
+                await db.collection('Case').updateOne({_id: CaseId}, {$push:{'Beweise.0.Bilder': {
+                    Bez: Bez,
+                    kurzbeschreibung: kurzbeschreibung,
+                }}}).then(
+                    res.status(201).send("Bild zu Fall hinzugefügt")
+                )
+    
+                break;
+            case "Zeuge":
+                const {VName, NName, Aussage, ZRelevanz} = req.body
+                await db.collection('Case').updateOne({_id: CaseId}, {$push:{'Beweise.0.Zeugenaussagen': {
+                    VName: VName,
+                    NName: NName,
+                    Aussage: Aussage,
+                    Relevanz: ZRelevanz
+                }}}).then(
+                    res.status(201).send("Zeuge zu Fall hinzugefügt")
+                )
+                break;
+            default:
+                console.log('Ist nichts gültiges')
 
+                break;
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(404).send("Fehler bei Anlage eines Beweises aufgetreten")
+    }
 })
 
 router.post('/:Aktenzeichen/Verknuepfen', async (req,res) => {
-    
+    try {
+        const {AKTZ, Begruendung} = req.body
+        const CaseId = req.params.Aktenzeichen;
+
+        await db.collection('Case').updateOne({_id: CaseId}, {$push:{VerknuepfteFaelle: {
+            Aktenzeichen: AKTZ,
+            Begruendung: Begruendung,
+        }}})
+        .then(
+            await db.collection('Case').updateOne({_id: AKTZ}, {$push:{VerknuepfteFaelle: {
+                Aktenzeichen: CaseId,
+                Begruendung: Begruendung,
+            }}})
+            .then(
+                res.status(200).send("Fälle verknüpft")
+            )
+        )
+    } catch (error) {
+        console.log(error)
+        res.status(404).send("Fehler bei Verknüpfung von Fällen aufgetreten")
+    } 
 })
+
+
 
 export default router
