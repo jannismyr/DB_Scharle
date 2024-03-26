5b86975507466366a6cac491c28723a9968cee82
Hoffi14 <S.hofmann03@web.de>
2024-03-26 01:31:10 +0100

    Erstellung neuer Anzeige nun funktionsfähig

--- frontend/src/seiten/NeueAnzeige.vue
+++ frontend/src/seiten/NeueAnzeige.vue
@@ -1,45 +1,151 @@
 <template>
     <div>
-        <h1>Anlegen einer neuen Anzeige</h1>
-
-<!-- Eingabefelder zum Erstellen einer Anzeige -->
-   <form @submit.prevent="validateForm" id="Form">
-<label for="caseName">Name:</label>
-<input type="text" id="caseName" name="caseName" v-model="this.Case.Name"  ><br><br>
-
-<label for="caseDate">Datum:</label>
-<input type="date" id="caseDate" name="caseDate" v-model="this.Case.Datum" required><br><br>
-
-<label for="caseLocation">Ort:</label>
-<input type="text" id="caseLocation" name="caseLocation" v-model="this.Case.Ort"><br><br>
-
-<label for="caseDescription">Beschreibung:</label><br>
-<textarea id="caseDescription" name="caseDescription" rows="4" cols="50" v-model="this.Case.Beschreibung"></textarea><br><br>
-
-<input type="submit"  value="Anzeige erstellen">
-</form>
-
+      <form name="Anzeigenerfassung" @submit.prevent="false">
+        <div class="button-container">
+          <Tabs :tabs="tabs">
+            <template #Allgemein>
+              <div>
+                <GeneralData @General-Daten="GetGeneralData"/>
+              </div>
+            </template>
+            <template #Opfer>
+              <div>
+                  <VictimData @Opfer-Daten="GetVictim"/>
+              </div>
+            </template>
+            <template #Täter>
+              <div>
+                <PerpetratorData @Taeter-Daten="GetPerpetrator"/>
+              </div>
+            </template>
+            <template #Geo>
+              <div>
+                <GeoData @Geo-Daten="GetGeoData"/>
+              </div>
+            </template>
+          </Tabs>
+        </div>
+      <button type="submit" class="SubmitButton" @click="submitForm">Hier bestätigen</button>
+      </form>
     </div>
-</template>
 
+
+</template>
+  
 <script>
-// import axios from 'axios'; 
+import axios from 'axios'
+
+import Tabs from '../components/AnzeigeErstellen/Tabs/TestTabsComp.vue';
+
+import GeneralData from '../components/AnzeigeErstellen/Comps/GeneralData.vue'
+import VictimData from '../components/AnzeigeErstellen/Comps/VictimData.vue'
+import PerpetratorData from '../components/AnzeigeErstellen/Comps/PerpetratorData.vue'
+import GeoData from '../components/AnzeigeErstellen/Comps/GeoData.vue'
+
 
 export default {
-    name: 'NeueAnzeige',
-    data() {
-        return {
-            Case: {
-                Ort: '',
-                Beschreibung: ''
-            }
-        }
+  name: 'App',
+  components: {
+    Tabs,
+    VictimData,
+    PerpetratorData,
+    GeoData,
+    GeneralData
+  },
+  methods:{
+    GetGeneralData(General){
+      this.General = General
     },
-    created(){
-    if (!sessionStorage.getItem('Nutzer'))
-    {
-      window.location.replace('/login')
+    GetVictim(Opfer){
+      this.Victim = Opfer
+    },
+    GetPerpetrator(Taeter){
+      this.Taeter = Taeter
+    },
+    GetGeoData(Geo){
+      this.Geo = Geo
+    },
+
+    submitForm(){
+      if (this.General && this.Victim && this.Taeter && this.Geo) {
+        axios.post('/case', {
+          Tatvorwurf: this.General.Tat,
+          Tatzeit: this.General.Tatzeit,
+          Erfasser: {
+            Erfasser_ID: sessionStorage.getItem('Nutzer').Id,
+            ErfasserName: sessionStorage.getItem('Nutzer').BName
+          },
+          Opfer: {
+            VNameOpfer: this.Victim.Vorname,
+            NNameOpfer: this.Victim.Nachname,
+            AWN_Opfer: this.Victim.Auweisnummer,
+            AdresseOpfer: this.Victim.Adresse,
+            TelNumOpfer: this.Victim.Telefonnummer
+          },
+          Taeter: {
+            VNameTaeter: this.Taeter.Vorname,
+            NNameTaeter: this.Taeter.Nachname,
+            AWN_Taeter: this.Taeter.Ausweisnummer
+          },
+          Ort:{
+            Bundesland: this.Geo.Bundesland,
+            Landkreis: this.Geo.Landkreis,
+            Ort: this.Geo.Ort, 
+            Tatort: this.Geo.Tatort
+          }
+        }).then(alert("Formular abgesendet"),
+        document.Anzeigenerfassung.reset(),
+        window.location.replace(`/${this.General.Tat}`))
+      } else {
+        alert("Eine Seite nicht ausgefüllt")
+      }
+        
     }
-  }
-}
+  },
+  data() {
+    return {
+      tabs: [
+        {
+          id: 'Allgemein',
+          title: 'Allgemeine Daten',
+          active: false,
+        },
+        {
+          id: 'Opfer',
+          title: 'Opfer',
+          active: true,
+        },
+        {
+          id: 'Täter',
+          title: 'Täter',
+          active: false,
+        },
+        {
+          id: 'Geo',
+          title: 'Geografische Daten',
+          active: false,
+        },
+        
+      ],
+      Victim: null,
+      Taeter:null,
+      Geo: null,
+      General: null
+    };
+  },
+};
 </script>
+
+<style>
+.button-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 2rem;
+  height: 55vh;
+}
+.SubmitButton {
+  margin-top: 3rem;
+  padding: 1rem;
+}
+
+</style>
