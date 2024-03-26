b443de2796a0076e57d6d904d42183486f368ec1
Hoffi14 <S.hofmann03@web.de>
2024-03-26 01:31:51 +0100

    Detailansicht. muss noch gestaltet werden

---
+++ frontend/src/components/DetailComp.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+       <div class="box">
+          <!-- Anzeige der Details der Anzeige -->
+         <div>
+             <h3>Tatvorwurf: {{ Tat }}</h3>
+             <h4>Datum: {{ new Date(Datum).toLocaleDateString() + " " +new Date(Datum).toLocaleTimeString()}}</h4>
+            
+   
+   
+         <!--<router-link :to="`/detailansicht/${veranstaltungId}`">
+           <button class="detail-button">Veranstaltung näher anzeigen</button>
+         </router-link>
+        
+         <p>Veranstaltung ID: {{ veranstaltungId }}</p>-->
+         </div>
+       </div>
+   
+     </div>
+   </template>
+   
+   <script>
+     import axios from 'axios';
+     export default {
+       name: "AnzeigeKomp",
+       props:{
+         Tat: String,
+         Datum: String
+       },
+       data() {
+           return{
+             Faelle: [],
+           }
+       },
+       methods: {
+       /*async deleteVeranstaltung() {
+         try {
+           // `veranstaltungId` wird als Prop übergeben
+           await axios.delete(`/api/veranstaltungen/${this.veranstaltungId}`);
+           window.location.reload();        
+         } catch (error) {
+           console.error("Fehler beim Löschen der Veranstaltung", error);
+         }
+       },*/
+       }
+     };
+   </script>
